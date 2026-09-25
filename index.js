c8a7c81285b493e50c53bb8b4d4756f087265215
codingwalalbhaiya <bhaiyacodewala@gmail.com>
2026-09-25 13:46:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
  
-  // Import the functions you need from the SDKs you need
+  
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
   import { getDatabase, ref, onValue, get} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
+ 
   const firebaseConfig = {
     apiKey: "AIzaSyB0HdY9mQuigeKYOAC4ZEDmTKPJ-s2BOQw",
     authDomain: "varshnay-gallery-db177.firebaseapp.com",
