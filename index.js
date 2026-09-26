b74995c9d452ee3550a2ca55d64aa3ae2d7d793f
codingwalalbhaiya <bhaiyacodewala@gmail.com>
2026-09-26 15:24:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -617,24 +617,6 @@ if (uploadArea) {
       reader.readAsDataURL(file);
       
 
-
- 
-function base64ToFile(base64, fileName){
-  let arr = base64.split(',');
-  let mime = arr[0].match(/:(.*?);/)[1];
-  let bstr = atob(arr[1]);
-  let n = bstr.length;
-  let u8arr = new Uint8Array(n);
-  while(n--){ u8arr[n] = bstr.charCodeAt(n);}
-  return new File([u8arr], fileName, {type:mime});
-}
-
-async function sendBothPhotos(){
-  let productFile = base64ToFile(clickedImgSrc, "product.png");
-  let fileToShare = [productFile, clientFile];
-  
-}
-
 if (navigation.canShare && navigator.canShare({files:[file]})){
   setTimeout(async ()=>{
     try{
